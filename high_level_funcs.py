e91944c9cd256a5fa5f9c0ed5f0f11454771d613
Eckart Cobo-Briesewitz <68149355+Tupryk@users.noreply.github.com>
2024-12-09 14:33:16 +0100

    ugly good funcs

--- high_level_funcs.py
+++ high_level_funcs.py
@@ -2,6 +2,8 @@ import numpy as np
 import robotic as ry
 from time import sleep
 
+import Robotic_Manipulation.manipulation as manip
+
 
 class RobotEnviroment:
     def __init__(self, C: ry.Config, visuals: bool=False, verbose: int=0):
@@ -9,75 +11,103 @@ class RobotEnviroment:
         self.visuals = visuals
         self.verbose = verbose
         self.grabbed_frame = ""
-
-
-    def basicKomo(self, C: ry.Config, phases: int=1) -> ry.KOMO:
-        komo = ry.KOMO(C, phases, 32, 2, True)
-        
-        komo.addControlObjective([], 0, 1e-2)
-        komo.addControlObjective([], 1, 1e-1)
-        komo.addControlObjective([], 2, 1e-2)
-
-        komo.addObjective([], ry.FS.accumulatedCollisions, [], ry.OT.eq, [1e1])
-        komo.addObjective([], ry.FS.jointLimits, [], ry.OT.ineq, [1e1])
-
-        return komo
-
-    def solve_and_run(self, komo: ry.KOMO, C: ry.Config, visuals: bool=False) -> bool:
-        sol = ry.NLP_Solver()
-        sol.setProblem(komo.nlp())
-        sol.setOptions(damping=1e-1, verbose=0, stopTolerance=1e-3, maxLambda=100., stopInners=20, stopEvals=200)
-        ret = sol.solve()
-
-        if not ret.feasible:
-            return False
-        
-        path = komo.getPath()
-        if visuals:
-            for q in path:
-                C.setJointState(q)
-                C.view()
-                sleep(.1)
-        else:
-            C.setJointState(path[-1])
-                
-        return True
+        self.path = np.array([])
 
     def pick(self, frame: str) -> bool:
 
-        komo = self.basicKomo(self.C)
-        komo.addObjective([1.], ry.FS.negDistance, ["l_gripper", frame], ry.OT.ineq, [-1e1], [.01])
-        komo.addObjective([1.], ry.FS.vectorZ, ["l_gripper"], ry.OT.eq, [1e1], [0., 0., 1.])
-
-        solved = self.solve_and_run(komo, self.C, self.visuals)
-        if not solved:
-            if self.verbose:
-                print("Pick was unsuccessful!")
+        graspDirection = np.random.choice(['x', 'y'])
+        gripper = "l_gripper"
+        table = "table"
+        palm = "l_palm"
+        box = frame
+        M = manip.ManipulationModelling()
+        M.setup_sequence(self.C, 1)
+        M.grasp_box(1., gripper, box, palm, graspDirection)
+        M.no_collisions([], [palm, table])
+        ways = M.solve()
+        if not M.feasible:
             return False
-        
-        self.C.attach("l_gripper", frame)
+
+        M2 = M.sub_motion(0)
+        M2.no_collisions([.3,.7], [palm, box], margin=.05)
+        M2.retract([.0, .2], gripper)
+        M2.approach([.8, 1.], gripper)
+        M2.solve()
+        if not M2.feasible:
+            return False
+
+        M2.play(C)
         self.grabbed_frame = frame
+        self.C.attach(gripper, frame)
         return True
 
     def place(self, x: float, y: float, z: float=.69) -> bool:
-         
-        komo = self.basicKomo(self.C)
-        komo.addObjective([1.], ry.FS.position, ["l_gripper"], ry.OT.eq, [1e1], [x, y, z])
-        komo.addObjective([1.], ry.FS.vectorZ, ["l_gripper"], ry.OT.eq, [1e1], [0., 0., 1.])
+        assert self.grabbed_frame != ""
+        placeDirection = np.random.choice(['x', 'y', 'z', 'xNeg', 'yNeg', 'zNeg'])
+        table = "table"
+        palm = "l_palm"
 
-        solved = self.solve_and_run(komo, self.C, self.visuals)
-        if not solved:
-            if self.verbose:
-                print("Place was unsuccessful!")
+        box = self.grabbed_frame
+        M = manip.ManipulationModelling()
+        M.setup_sequence(self.C, 1)
+        M.place_box(1., box, table, palm, placeDirection)
+        M.no_collisions([], [palm, table])
+        M.target_relative_xy_position(1., box, table, [x, y])
+        ways = M.solve()
+        if not M.feasible:
             return False
-        
-        if self.grabbed_frame:
-            self.grabbed_frame = ""
-            self.C.attach("table", self.grabbed_frame)
+
+        M3 = M.sub_motion(0)
+        M3.no_collisions([], [table, box])
+        M3.solve()
+        if not M3.ret.feasible:
+            return False
+
+        M3.play(C)
+        C.attach(table, box)
+        self.grabbed_frame = ""
         return True
 
     def push(self, frame: str, relative_x: float, relative_y: float) -> bool:
-        pass
+
+        obj_pos = self.C.getFrame(frame).getPosition()
+        target_pos = obj_pos + np.array([relative_x, relative_y, 0.])
+        
+        gripper = "l_gripper"
+        table = "table"
+        obj = frame
+        M = manip.ManipulationModelling()
+        M.setup_pick_and_place_waypoints(self.C, gripper, obj, 1e-1, accumulated_collisions=False)
+        pushStart = M.straight_push([1.,2.], obj, gripper, table)
+        M.komo.addObjective([2.], ry.FS.position, [obj], ry.OT.eq, 1e1*np.array([[1,0,0],[0,1,0]]), target_pos)
+        M.solve()
+        if not M.ret.feasible:
+            return False
+
+        M1 = M.sub_motion(0, accumulated_collisions=False)
+        M1.retractPush([.0, .15], gripper, .03)
+        M1.approachPush([.85, 1.], gripper, .03)
+        M1.no_collisions([.15,.85], [obj, 'l_finger1'], .02)
+        M1.no_collisions([.15,.85], [obj, 'l_finger2'], .02)
+        M1.no_collisions([.15,.85], [obj, 'l_palm'], .02)
+        M1.no_collisions([], [table, 'l_finger1'], .0)
+        M1.no_collisions([], [table, 'l_finger2'], .0)
+        M1.solve()
+        if not M1.ret.feasible:
+            return False
+
+        M2 = M.sub_motion(1, accumulated_collisions=False)
+        M2.komo.addObjective([], ry.FS.positionRel, [gripper, pushStart], ry.OT.eq, 1e1*np.array([[1,0,0],[0,0,1]]))
+        M2.solve()
+        if not M2.ret.feasible:
+            return False
+
+        M1.play(C, 1.)
+        C.attach(gripper, obj)
+        M2.play(C, 1.)
+        C.attach(table, obj)
+
+        return True
 
 
 if __name__ == "__main__":
